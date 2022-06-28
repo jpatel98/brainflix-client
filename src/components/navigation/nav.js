190969aa2e7a56203ae80acd68a2b7789448f858
Jigar Patel <jigar@JP-machine.local>
2022-06-28 11:07:17 -0400

    Finish navbar

---
+++ src/components/navigation/nav.js
@@ -0,0 +1,18 @@
+import logo from '../../assets/logo/BrainFlix-logo.svg';
+import userImg from '../../assets/images/Mohan-muruge.jpg'
+import './_nav.scss'
+
+const nav = () => {
+    return (
+        <nav className='navbar'>
+            <img src={logo} alt="BrainFlix logo" className='navbar__img'/>
+            <div className='navbar__search'>
+                <input type="text" placeholder='Search' className='navbar__search-input'></input>
+                <img src={userImg} alt="User profile" className='navbar__search-userImg'/>
+            </div>
+            <div className='navbar__upload'>UPLOAD</div>
+        </nav>
+    );
+};
+
+export default nav;
