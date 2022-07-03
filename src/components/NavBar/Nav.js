3d1699d188ff3b376b95ec6b69a08a51e82fc162
Jigar Patel <jigar@JP-machine.local>
2022-07-03 11:13:58 -0400

    Add link to navbar logo

--- src/components/NavBar/Nav.js
+++ src/components/NavBar/Nav.js
@@ -5,7 +5,7 @@ import './_nav.scss'
 const nav = () => {
     return (
         <nav className='navbar'>
-            <img src={logo} alt="BrainFlix logo" className='navbar__img'/>
+            <a href='./'><img src={logo} alt="BrainFlix logo" className='navbar__img'/></a>
             <div className='navbar__search'>
                     <input type="text" placeholder='Search' className='navbar__search-input'></input>
                     <img src={userImg} alt="User profile" className='navbar__search-userImg'/>
