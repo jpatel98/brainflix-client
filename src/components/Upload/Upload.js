aeb02b10f4ccb4a1f82541beedc6255ed873e04a
Jigar Patel <jigar@JP-machine.local>
2022-07-07 10:46:52 -0400

    Fix typo

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -8,16 +8,16 @@ function Upload() {
       <div className="upload__margin">
         <h1 className="upload__title page-title">Upload Video</h1>
         <form className="upload__flex">
-          {/* <div className="upload__flex-one"> */}
+          <div className="upload__flex-one">
             <h4 className="upload__description section-subheader">VIDEO THUMBNAIL</h4>
             <img className="upload__image" src={thumbnail} alt="thumbnail" />
-          {/* </div> */}
-          {/* <div className="upload__flex-two"> */}
+          </div>
+          <div className="upload__flex-two">
             <label className="upload__description section-subheader" htmlFor='title'>TITLE YOUR VIDEO</label>
             <input className="upload__form-title body-copy" name="title" id="title" placeholder="Add a title to your video" />
             <label className="upload__description section-subheader" htmlFor='description'>ADD A VIDEO DESCRIPTION</label>
             <textarea className="upload__form-description body-copy" name="description" id="description" placeholder="Add a description of your video"/>
-          {/* </div> */}
+          </div>
         </form>
         <div className="upload__button-container">
           <button className="upload__button-publish button-text">PUBLISH</button>
