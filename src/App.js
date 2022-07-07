6a0173f49f27c3df1cd2a0b2f6a56f38d5f21c3b
Jigar Patel <jigar@JP-machine.local>
2022-07-07 09:48:17 -0400

    Upload page ready for mobile, tablet and pc layout, albeit minor fixes remain

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/Nav';
+import Upload from './components/Upload/Upload';
 import Hero from './components/Hero/Hero';
 import vidDataDetails from './data/video-details.json';
 import vidData from './data/videos.json';
 import VideoInfo from './components/VideoInfo/VideoInfo';
 import Comments from './components/Comments/Comments';
 import VideoList from './components/VideoList/VideoList'
+import './App.scss';
 
 
 class App extends React.Component {
@@ -25,22 +27,27 @@ class App extends React.Component {
       currentVid: selectedVideo
     })
   }
-
+  
   render(){
    // filtering through the videos to find the current video based on the id
     const filteredVid = this.state.videos.filter(video => video.id !== this.state.currentVid.id);
     return (
-      <>
+      <Router>
         <NavBar/>
-        <Hero currentVid={this.state.currentVid}/>
-        <div className='div1'>
-          <div className='div2'>
-            <VideoInfo currentVid={this.state.currentVid}/>
-            <Comments currentVid={this.state.currentVid}/>
-          </div>
-          <div className='div3'><VideoList vidList = {filteredVid} onSelectVid = {this.selectVideo}/></div>
-        </div>
-      </>
+        <Switch>
+          <Route path='/' exact>
+            <Hero currentVid={this.state.currentVid}/>
+            <div className='div1'>
+              <div className='div2'>
+                <VideoInfo currentVid={this.state.currentVid}/>
+                <Comments currentVid={this.state.currentVid}/>
+              </div>
+              <div className='div3'><VideoList vidList = {filteredVid} onSelectVid = {this.selectVideo}/></div>
+            </div>
+          </Route>
+          <Route path='/upload' exact component={Upload} />
+        </Switch>
+      </Router>
     );
   }
 };
--- src/components/NavBar/Nav.js
+++ src/components/NavBar/Nav.js
@@ -1,16 +1,19 @@
 import logo from '../../assets/logo/BrainFlix-logo.svg';
 import userImg from '../../assets/images/Mohan-muruge.jpg'
+import { NavLink } from 'react-router-dom';
 import './_nav.scss'
 
 const nav = () => {
     return (
         <nav className='navbar'>
-            <a href='./'><img src={logo} alt="BrainFlix logo" className='navbar__img'/></a>
+            <NavLink to="/" exact>
+                <img src={logo} alt="BrainFlix logo" className='navbar__img'/>
+            </NavLink>
             <div className='navbar__search'>
                     <input type="text" placeholder='Search' className='navbar__search-input'></input>
-                    <img src={userImg} alt="User profile" className='navbar__search-userImg'/>
+                        <img src={userImg} alt="User profile" className='navbar__search-userImg'/>
             </div>
-            <button className='navbar__upload button-text'>UPLOAD</button> 
+            <NavLink to="/upload" exact className='navbar__upload button-text'>UPLOAD</NavLink> 
         </nav>
     );
 };
