60a719e497256835cabf59d373e67d9151b1ae02
Jigar Patel <jigar@JP-machine.local>
2022-07-09 18:17:02 -0400

    Font fix in videolist section

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './VideoList.scss';
+import './VideoList.scss'
 
 const VideoList = props => {
     const vidListArr = props.vidList;
@@ -16,7 +16,7 @@ const VideoList = props => {
             <NavLink to={`/videos/${nested.id}`}>
                 <div><img src={nested.image} alt="placeholder" className='vidlist__container-item-image'/></div>
                 <div className='vidlist__container-item-details'>
-                    <p className='vidlist__container-item-details-title body-copy'>{nested.title}</p>
+                    <p className='vidlist__container-item-details-title'>{nested.title}</p>
                     <p className='vidlist__container-item-details-channel body-copy'>{nested.channel}</p>
                 </div>
             </NavLink>
